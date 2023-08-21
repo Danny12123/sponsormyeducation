bfb8a244a882025f114f1b17a642790c22c0738b
Danny <74186971+Danny38-pag@users.noreply.github.com>
2023-08-21 14:52:50 +0000

    jhh

--- src/firebase/index.js
+++ src/firebase/index.js
@@ -5,23 +5,14 @@ import "firebase/firestore"
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
-// const firebaseConfig = {
-//   apiKey: "AIzaSyD8P1Hl0mnCH8B7LkcoCQ2Hf2CueAQyoB8",
-//   authDomain: "try2-edc31.firebaseapp.com",
-//   projectId: "try2-edc31",
-//   storageBucket: "try2-edc31.appspot.com",
-//   messagingSenderId: "947272246400",
-//   appId: "1:947272246400:web:f3bc8c16eac3c4abfe7265",
-//   measurementId: "G-B8PE19QFVM"
-// };
 const firebaseConfig = {
-  apiKey: "AIzaSyCLY2LS7XkyeDFV_3ADAexonk_T_UeSL3w",
-  authDomain: "gofundme-5e71d.firebaseapp.com",
-  projectId: "gofundme-5e71d",
-  storageBucket: "gofundme-5e71d.appspot.com",
-  messagingSenderId: "354991817310",
-  appId: "1:354991817310:web:13077edbf8d5b2ac10aacf",
-  measurementId: "G-J5QY0C1DM8"
+  apiKey: "AIzaSyD4kJt7rxL5iIFlkjSsBN0QjTLB0A0GGAg",
+  authDomain: "myeducation-2a61d.firebaseapp.com",
+  projectId: "myeducation-2a61d",
+  storageBucket: "myeducation-2a61d.appspot.com",
+  messagingSenderId: "886077075456",
+  appId: "1:886077075456:web:f12f1c1f99da8e2b4d68e5",
+  measurementId: "G-YT12QNY4WV"
 };
 
 if (!initializeApp.length) {
